c84e5dc84c3c7531a19e8ca8ebc812306688c52b
Your Name <you@example.com>
2024-03-20 21:15:47 +0300

    test

--- app.py
+++ app.py
@@ -1,44 +1,66 @@
-from datetime import datetime
+"""Точка входа в приложение."""
 
-from utils import (
-    write_tasks,
-    read_tasks,
-    filter_tasks
-)
+from datetime import datetime, timezone
+
 from fastapi import (
     FastAPI,
     Request,
 )
 from fastapi.responses import JSONResponse as Response
 
+from utils import filter_tasks, read_tasks, write_tasks
+
 app = FastAPI()
 
 
-@app.get('/')
+@app.get("/")
 def get(r: Request) -> Response:
-    priority = r.query_params.get('priority')
+    """_summary_.
+
+    Args:
+    ----
+        r (Request): _description_
+
+    Returns:
+    -------
+        Response: _description_
+
+    """
+    priority = r.query_params.get("priority")
     data = read_tasks()
     if priority:
         data = filter_tasks(data, int(priority))
     return Response(content=data, status_code=200)
 
 
-@app.post('/')
+@app.post("/")
 async def post(r: Request) -> Response:
+    """_summary_.
+
+    Args:
+    ----
+        r (Request): _description_
+
+    Returns:
+    -------
+        Response: _description_
+
+    """
     data = await r.json()
-    errors = []
-    for item in ('title', 'description', 'priority'):
-        if not data.get(item):  # not
-            errors.append(f"Нет поля {item}")
-    if not isinstance(data.get('priority'), int):  # и тут
+    errors = [
+        f"Нет поля {item}"
+        for item in ("title", "description", "priority")
+        if not data.get(item)
+    ]
+    if not isinstance(data.get("priority"), int):  # и тут
         errors.append("Поле priority должно быть числом")
     if errors:
         return Response(content=errors, status_code=400)
     task = {
-        'title': data.get('title'),
-        'description': data.get('description'),
-        'priority': int(data.get('priority')),
-        'date': datetime.now().strftime('%Y.%m.%d %H:%M')
+        "title": data.get("title"),
+        "description": data.get("description"),
+        "priority": int(data.get("priority")),
+        "date": datetime.now(tz=timezone.utc).strftime("%Y.%m.%d %H:%M"),
     }
     tasks = read_tasks()
     tasks.append(task)
@@ -46,22 +68,25 @@ async def post(r: Request) -> Response:
     return Response(content=task, status_code=201)
 
 
-@app.delete('/')
+@app.delete("/")
 def delete(r: Request) -> Response:
-    title = r.query_params.get('title')
+    """safdsaf.
+
+    Args:
+    ----
+        r (Request): _description_
+
+    Returns:
+    -------
+        Response: _description_
+
+    """
+    title = r.query_params.get("title")
     if not title:
-        return Response(content=['Нет поля title'], status_code=400)
+        return Response(content=["Нет поля title"], status_code=400)
     tasks = read_tasks()
-    new_tasks = [
-        task
-        for task in tasks
-        if task.get('title') != title
-    ]
+    new_tasks = [task for task in tasks if task.get("title") != title]
     if len(new_tasks) == len(tasks):
-        return Response(content=['Нет такой task'], status_code=404)
+        return Response(content=["Нет такой task"], status_code=404)
     write_tasks(new_tasks)
     return Response(content=None, status_code=204)
-
-
-
-
--- utils.py
+++ utils.py
@@ -1,22 +1,56 @@
+"""Служебный функции."""
+
 import json
+from pathlib import Path
 
-PATH = 'tasks.json'
+PATH = "tasks.json"
 
 
-def write_tasks(tasks: list[dict[str, str | int]], path=PATH):
-    with open(path, 'w', encoding='utf-8') as f:
+def write_tasks(tasks: list[dict[str, str | int]], path: str = PATH) -> None:
+    """_summary_.
+
+    Args:
+    ----
+        tasks (list[dict[str, str  |  int]]): _description_
+        path (str, optional): _description_. Defaults to PATH.
+
+    """
+    path = Path.resolve.parent / path
+    with path.open("w", encoding="utf-8") as f:
         json.dump(tasks, f, ensure_ascii=False, indent=4)
 
 
-def read_tasks(path=PATH) -> list[dict[str, str | int]]:
-    with open(path, 'r', encoding='utf-8') as f:
+def read_tasks(path: str = PATH) -> list[dict[str, str | int]]:
+    """_summary_.
+
+    Args:
+    ----
+        path (str, optional): _description_. Defaults to PATH.
+
+    Returns:
+    -------
+        list[dict[str, str | int]]: _description_
+
+    """
+    path = Path.resolve.parent / path
+    with path.open(encoding="utf-8") as f:
         return json.load(f)
 
 
-def filter_tasks(tasks: list[dict[str, str | int]], priority: int) \
-        -> list[dict[str, str | int]]:
-    return [
-        task
-        for task in tasks
-        if task.get('priority') == priority
-    ]
+def filter_tasks(
+    tasks: list[dict[str, str | int]],
+    priority: int,
+) -> list[dict[str, str | int]]:
+    """_summary_.
+
+    Args:
+    ----
+        tasks (list[dict[str, str  |  int]]): _description_
+        priority (int): _description_
+
+    Returns:
+    -------
+        list[dict[str, str | int]]: _description_
+
+    """
+    return [task for task in tasks if task.get("priority") == priority]
